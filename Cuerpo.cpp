46f28496939313bc20be0a190ae34139648bb946
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-10-29 11:54:18 +0100

    Initial classes

--- Cuerpo.cpp
+++ Cuerpo.cpp
@@ -1,10 +1,15 @@
 class Cuerpo{
     public:
-        double posX, posY, mass;
+        double posX, posY, masa;
 
         void init(unsigned int seed);
         
 };
 
 class Planeta : public Cuerpo {  
-}
+
+};
+
+class Asteroide : public Cuerpo {
+
+};
