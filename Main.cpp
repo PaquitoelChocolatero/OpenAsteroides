3b0f5112361f57168cf396c8f9de2f0b580524ec
Juan Francisco Garcia <juanfgarciacas@gmail.com>
2019-10-29 15:56:24 +0100

    Valores inicializados correctamente

--- Main.cpp
+++ Main.cpp
@@ -38,15 +38,25 @@ void init(unsigned int seed, Cuerpo &c){
 int main() {
     int num_asteroides = 5;
     int num_planetas = 1;
+    int semilla = 2000;
 
     Cuerpo cuerpos[num_asteroides + num_planetas];
     Planeta planetas[num_planetas];
     Asteroide asteroides[num_asteroides];
 
-    for (int i = 0; i<20;i++){
-         init(2000+i, c);
-         cout << c.posX << ", " << c.posY << ", " << c.masa << endl;
+    for (int i = 0;i<sizeof(cuerpos); i++){
+        if (i<num_asteroides){
+            init(semilla+i, asteroides[i]);
+        }
+        else {
+            init(semilla+i, planetas[i-num_asteroides]);
+        }
     }
+
+    for (int i = 0; i<sizeof(num_asteroides); i++){
+        cout << asteroides[i].posX << ", " <<  asteroides[i].posY << ", " << asteroides[i].masa << endl;
+    }
+
    
 
 }
