3fe08dd9aa35831c9d9692958256f72febe82837
Juan Francisco Garcia <juanfgarciacas@gmail.com>
2019-10-30 08:54:29 +0100

    Init terminado

--- Main.cpp
+++ Main.cpp
@@ -19,7 +19,7 @@ class Asteroide : public Cuerpo {
 };
 
 
-void init(unsigned int seed, Cuerpo &c){
+void init(unsigned int seed, Cuerpo &c, int planeta){
     int width = 200;
     int height = 200;
     double mass = 1000;
@@ -29,33 +29,54 @@ void init(unsigned int seed, Cuerpo &c){
     std::uniform_real_distribution<double> xdist{0.0, std::nextafter(width, std::numeric_limits<double>::max())};
     std::uniform_real_distribution<double> ydist{0.0, std::nextafter(height,std::numeric_limits<double>::max())};
     std::normal_distribution<double> mdist{mass, sdm};
-
-   
-    c.set(xdist(re), ydist(re), mdist(re));
-
+    
+    double posX = xdist(re);
+    double posY = ydist(re);
+    double masa = mdist(re);    
+    if (planeta != -1) {
+        planeta = planeta % 4;
+        switch (planeta){
+            case 0:
+                posX = 0;
+                break;
+            case 1:
+                posY = 0;
+                break;
+            case 2:
+                posX = width;
+                break;
+            case 3:
+                posY = height;
+                break;
+        }
+    }
+    c.set(posX, posY, masa);
 }
 
 int main() {
     int num_asteroides = 5;
-    int num_planetas = 1;
+    int num_planetas = 5;
     int semilla = 2000;
 
-    Cuerpo cuerpos[num_asteroides + num_planetas];
+    //Cuerpo cuerpos[num_asteroides + num_planetas];
     Planeta planetas[num_planetas];
     Asteroide asteroides[num_asteroides];
 
     for (int i = 0;i<(num_asteroides + num_planetas); i++){
         if (i<num_asteroides){
             cout << "Inicializado asteroide " << i<<endl;
-            init(semilla+i, asteroides[i]);
+            init(semilla+i, asteroides[i], -1);
         }
         else {
             cout << "Inicializado planeta" << i-num_asteroides<<endl;
-            init(semilla+i, planetas[i-num_asteroides]);
+            init(semilla+i, planetas[i-num_asteroides], i-num_asteroides);
         }
     }
 
-    for (int i = 0; i< num_asteroides; i++){
+     for (int i = 0; i< num_asteroides; i++){
         cout << asteroides[i].posX << ", " <<  asteroides[i].posY << ", " << asteroides[i].masa << endl;
+    }
+    for (int i = 0; i< num_planetas; i++){
+        cout << planetas[i].posX << ", " <<  planetas[i].posY << ", " << planetas[i].masa << endl;
     }
 }
