3051c85321dd98123d3de9600a45e1519af37e75
Juan Francisco Garcia <juanfgarciacas@gmail.com>
2019-11-01 18:59:34 +0100

    Segundo test

--- nasteroids-seq_test.cpp
+++ nasteroids-seq_test.cpp
@@ -14,8 +14,21 @@ TEST( Test_del_paso_de_argumentos, Sin_parametros){
             SUCCEED();
         }
     }
-    
+}
 
+TEST( Test_del_paso_de_argumentos, Parametros_no_integer){
+    int argc = 5;
+    char **argv = new char*[argc];
+    argv[1] = "hola";
+    
+    Datos d;
+    try {
+        d = parseArgs(argc, argv);
+    }catch (int i){
+        if (i == 1){
+            SUCCEED();
+        }
+    }
 }
 
 int main(int argc, char **argv){
