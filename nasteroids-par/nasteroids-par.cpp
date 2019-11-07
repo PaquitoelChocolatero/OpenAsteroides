bcddad2c920b96c3b58ed46c1e423e6fa347ed49
Juan Francisco Garcia <juanfgarciacas@gmail.com>
2019-11-07 17:10:53 +0100

    mas hilos mas hilos

--- nasteroids-par/nasteroids-par.cpp
+++ nasteroids-par/nasteroids-par.cpp
@@ -157,6 +157,8 @@ Datos parseArgs(int argc, char *argv[]){
 }
 
 int main(int argc, char *argv[]){
+
+    int num_threads = 20;
       
     Datos datos;
     try{
@@ -185,7 +187,7 @@ int main(int argc, char *argv[]){
         //Calculo de la fuerza de atracción entre cada cuerpo -> O(n_asteroides²-n_asteroides+n_asteroides*n_planetas)
         
         //Cada hilo recorre una porcion del array de cuerpos
-        #pragma omp parallel for
+        #pragma omp parallel for num_threads(num_threads)
         
             //Recorremos sólo los asteroides (los planetas son fijos)
             for(int i=0; i<datos.num_asteroides; i++){
